9094967c892a6fdbe87365ad0a85efdf8bf4251d
cmstexas <crystal.m.soria@gmail.com>
2019-07-16 17:10:02 -0500

    still troubleshooting

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -3,18 +3,28 @@ import React, {useState}  from "react";
 //import any components needed
 
 // import {} from '/GitWorkingFiles/lambda-calculator/src/data';
-import {specials} from '/../../../data';
+import { specials } from '/../../../data';
 
 //Import your array data to from the provided data file
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
 
+  const [funcSpecials, setSpecials] = useState(specials);
+
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       funcSpecials.map((special, index) => (
+        <SpecialButton special={special} key={index} />
+      ))
+       
+       }
     </div>
   );
 };
+
+export default Specials;
